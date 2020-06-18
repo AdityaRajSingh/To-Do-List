666ad1cce763cb9af74d4098c9873c0711676318
AdityaRajSingh <adi4adityaraj@gmail.com>
2020-06-19 00:44:21 +0530

    Updates

---
+++ precache-manifest.e56ecc32b7f30e5498e77d87d15bbda3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd9f52ba4c073619ff67ad0ff474f87",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "131d038086ba26544012",
+    "url": "/To-Do-List/static/js/2.6ad8c12a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.6ad8c12a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72bd1db1b0f3de803ed",
+    "url": "/To-Do-List/static/js/main.a0817839.chunk.js"
+  },
+  {
+    "revision": "3fc2ca48924706e73961",
+    "url": "/To-Do-List/static/js/runtime-main.ce724f51.js"
+  }
+]);
